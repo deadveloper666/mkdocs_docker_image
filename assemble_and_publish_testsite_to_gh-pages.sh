1ced8762fc11a229448407f3bbea053ccccc420a
Daniel Peña Perez <danipenaperez@gmail.com>
2023-11-21 16:29:34 +0100

    fixed script

--- assemble_and_publish_testsite_to_gh-pages.sh
+++ assemble_and_publish_testsite_to_gh-pages.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+git checkout gh-pages
+git merge main --no-ff
+# Assemble dist
 cd test
 ./documentationServer.sh assemble
 cd ..
-git checkout gh-pages
-git merge main --no-ff
 cp -a ./test/site/. .
+
